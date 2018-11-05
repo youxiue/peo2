e01d2f7568c8033b669c32f14458cd8830a6c0db
itheima <xfbliuli@qq.com>
2018-11-05 21:45:12 +0800

    创建第二个测试

---
+++ git_test_protect/src/main/java/com/itheima/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("shenme");
+        System.out.println("shenme");
+        System.out.println("shenme");
+        System.out.println("shenme");
+
+    }
+}
