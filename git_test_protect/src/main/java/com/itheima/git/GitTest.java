169f5300df3a56f766d40b9ad03dcc8e142ac98a
itheima <xfbliuli@qq.com>
2018-11-05 22:17:12 +0800

    创建第二个测试

--- git_test_protect/src/main/java/com/itheima/git/GitTest.java
+++ git_test_protect/src/main/java/com/itheima/git/GitTest.java
@@ -6,6 +6,9 @@ public class GitTest {
         System.out.println("shenme");
         System.out.println("shenme");
         System.out.println("shenme");
-
+    }
+    public void test (){
+        int i =0;
+        i = 10;
     }
 }
