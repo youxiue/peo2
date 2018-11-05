c0f8040f0615b37d9b5d3ab7ed1daffe821f93bc
itheima <xfbliuli@qq.com>
2018-11-05 21:23:56 +0800

    添加工程

---
+++ gittestprotected/src/main/java/com/itheima/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+        System.out.println("hahahahahahahahahahahah");
+    }
+    public void test(){
+        int i = 10;
+        i +=100;
+        System.out.println(i);
+    }
+}
